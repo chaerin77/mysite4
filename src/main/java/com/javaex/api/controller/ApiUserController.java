6e244eaaa324fe22cd0985b94397bf10fb564e6a
chaerin <77728068+Boram8878@users.noreply.github.com>
2022-02-02 23:16:01 +0900

    mysite4 ajax json 중복체크

--- src/main/java/com/javaex/api/controller/ApiUserController.java
+++ src/main/java/com/javaex/api/controller/ApiUserController.java
@@ -1,11 +1,7 @@
 package com.javaex.api.controller;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
