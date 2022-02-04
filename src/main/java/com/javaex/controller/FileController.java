148e4f2e51beaf177b2af6acf93f113a05142e33
chaerin <77728068+Boram8878@users.noreply.github.com>
2022-02-04 23:46:27 +0900

    mysite4 file

---
+++ src/main/java/com/javaex/controller/FileController.java
@@ -0,0 +1,44 @@
+package com.javaex.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.javaex.service.FileService;
+
+@Controller
+@RequestMapping("/fileupload")
+public class FileController {
+
+	@Autowired
+	private FileService fileService;
+	
+	//파일업로드 폼
+	@RequestMapping("/form")
+	public String form() {
+		System.out.println("FileController.form()");
+		
+		
+		return "fileupload/form";
+	}
+
+	//파일업로드 처리
+	@RequestMapping("/upload")
+	public String result(@RequestParam("file") MultipartFile file, Model model) {
+		System.out.println("FileController.upload()");
+		/*
+		System.out.println(file);//이걸로 찍으면 실제로 데이터 안들어갔는데도 데이터아닌 다른정보들 모아 출력되어서 이걸로는 정보가 들어간건지 아닌지 알수없음
+		System.out.println(file.getOriginalFilename());//중요 데이터 넣은거 이걸로 확인
+		System.out.println(file.getSize());
+		*/
+		
+		String saveName = fileService.restore(file);
+		model.addAttribute("saveName", saveName);
+		
+		return "fileupload/result";
+	}
+	
+}
---
+++ src/main/java/com/javaex/service/FileService.java
@@ -0,0 +1,61 @@
+package com.javaex.service;
+
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.UUID;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class FileService {
+
+	public String restore(MultipartFile file) {
+		System.out.println("FileService.restore()");
+		String saveDir = "D:\\javaStudy\\upload";
+		
+		System.out.println(file.getOriginalFilename());
+		
+		//파일을 하드디스크에 저장(운영내용)
+		
+		//파일관련 정보 추출
+		//원본 파일 이름 --나중에 다운로드받을때 원본파일 이름으로 다시 줘야하니까 원본파일이름뽑아서 관리하기로함
+		String orgName = file.getOriginalFilename();
+		
+		//확장자
+		String exName = file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf("."));  //subString ->내가 알려준 index이후의 값을 갖고오라
+		
+		//저장파일이름
+		String saveName = System.currentTimeMillis()+ UUID.randomUUID().toString() + exName; //이미대문자 -> 스태틱에있는.. 뒤에 확장자까지 붙여줌  -->하드디스크에 저장될 파일의 이름 
+		System.out.println(saveName);
+		
+		//파일패스생성- 실제 파일 위치(경로)
+		String filePath = saveDir + "\\" + saveName; //경로가 바뀌게될때 대비해 경로를 변수로 빼놓기-6
+		
+		//파일 사이즈
+		long fileSize = file.getSize(); //자료형 long형이라 long으로 받아야함
+		//
+		
+		//파일 저장(업로드) -사용자입장에서 업로드
+		try {
+			
+			byte[] fileData = file.getBytes();
+			OutputStream out = new FileOutputStream(filePath);
+			BufferedOutputStream bout = new BufferedOutputStream(out);
+			
+			bout.write(fileData);
+			bout.close();
+			
+		}catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		
+		//DB에 저장 -테이블만들기 과제
+		
+		return saveName;
+	}
+	
+}
